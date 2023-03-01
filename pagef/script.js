a2bb404d245ddadbd898197721351b0ae0c66a25
Ken883 <68206257+Ken883@users.noreply.github.com>
2023-02-28 22:33:28 -0800

    Add files via upload

---
+++ pagef/script.js
@@ -0,0 +1,11 @@
+const guessInput = document.getElementById("User-input");
+const submitButton = document.getElementById("submit-button");
+const resultParagraph = document.getElementById("result");
+
+submitButton.addEventListener("click", function() {
+  
+  const userInput = parseInt(Input.value);
+
+  
+ 
+});
